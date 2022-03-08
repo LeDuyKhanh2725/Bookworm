f274678f2dabd5bc48b4cf6371c84d69bc19274d
Duy Khanh <leduykhanh2725@fpt.edu.vn>
2022-03-08 21:08:21 +0700

    return Home

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Welcome from './welcome';
+import Header from './component/header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/home/Home';
+import Carousel from './pages/home/Carousel';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Home />
+      {/* <Carousel /> */}
+    </React.Fragment>
+
+    
+  )
+}
+
+
 
 ReactDOM.render(
-  <Welcome/>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+
   document.getElementById('root')
 );
