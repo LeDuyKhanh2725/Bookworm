dab23686277ba019ef77918d61d744f7353141dd
Duy Khanh <leduykhanh2725@fpt.edu.vn>
2022-03-10 19:25:10 +0700

    write function to get api(developing)

--- resources/js/pages/shop/Shop.js
+++ resources/js/pages/shop/Shop.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 function Shop() {
+  const[state,setState] = useState({
+    listbook: [],
+    listsortonsale: [],
+    listsortpopular: [],
+    listsortlowhigh: [],
+    listsorthighlow: [],
+    listfiltercate: [],
+    listfilterauthor: []
+  });
+  useEffect(async () =>{
+    // const getData = await axios.get('http://127.0.0.1:8000/api/book-list');
+    const getDataonsale = await axios.get('');
+    const getDataPopular = await axios.get('');
+    const getDatalow = await axios.get('');
+    const getDatahigh = await axios.get('');
+    const getDatacate = await axios.get('');
+    const getDataauthor = await axios.get('');
+  setState({listbook:getData.data,listsortonsale:getDataonsale.data,
+  listsortpopular:getDataPopular.data, listsortlowhigh:getDatalow.data,
+listsorthighlow:getDatahigh.data,listfiltercate:getDatacate.data,
+listfilterauthor:getDataauthor.data})
+  },[])
   return (
-    <div>Shop</div>
+    <>
+      {/* <Shop list={state.listbook}/> */}
+    </>
   )
 }
 
