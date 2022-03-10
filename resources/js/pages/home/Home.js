293907401115ddaff1942f5a95341d65c290594f
Duy Khanh <leduykhanh2725@fpt.edu.vn>
2022-03-10 19:24:22 +0700

    get feature book

--- resources/js/pages/home/Home.js
+++ resources/js/pages/home/Home.js
@@ -1,23 +1,38 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import Carousel from 'react-bootstrap/Carousel'
+import { Button, ButtonGroup } from 'react-bootstrap';
+import Carousel from '../home/Carousel'
+import Featurebook from './featurebook/Featurebook'
 function Home() {
   const [state, setState] = useState({
-    listBook: []
+    listBook: [],
+    listRecommentbook: [],
+    listPopularbook: []
   });
+
+  console.log(state);
   useEffect(async () => {
     const getData = await axios.get('http://127.0.0.1:8000/api/book-most-discount');
+    const getDataRecomment = await axios.get('http://127.0.0.1:8000/api/book-most-recomment');
+    const getDataPopular = await axios.get('http://127.0.0.1:8000/api/book-most-popular');
 
-    setState({ listBook: getData.data })
+    setState({ listBook: getData.data, listRecommentbook: getDataRecomment.data,
+    listPopularbook: getDataPopular.data })
   }, [])
 
   return (
     <>
       <Carousel list={state.listBook} />
-      <button>123</button>
+     <br></br>
+      <Featurebook list={state.listRecommentbook}/>
+      <Featurebook list={state.listPopularbook}/>
       </>
       
+      
   )
+
+
+  
 }
 
 export default Home
