ed04659540605c801f353b5f665b2e98d7539c35
Duy Khanh <leduykhanh2725@fpt.edu.vn>
2022-03-13 22:21:08 +0700

    format

--- resources/js/pages/home/featurebook/Featurebook.js
+++ resources/js/pages/home/featurebook/Featurebook.js
@@ -6,13 +6,16 @@ function Featurebook({ list }) {
         return list.map((item) => {
             return (
                 <Card className='col-md-3 me-3' style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={"/bookcover/" + item.book_cover_photo + ".jpg"} />
+                    <Card.Img height={'250px'} variant="top" src={"/bookcover/" + item.book_cover_photo + ".jpg"} />
                     <Card.Body>
-                        <Card.Title>{item.book_title}</Card.Title>
-                        <Card.Text>
+                        <Card.Title height={'50px'}>{item.book_title}</Card.Title>
+                        <Card.Text height={'50px'}>
                             {item.author_name}
                         </Card.Text>
-                        <Button variant="primary">{item.book_price}/{item.discount_price}</Button>
+                        <Card.Text height={'50px'}>
+                            {item.book_price}/{item.discount_price}
+                        </Card.Text>
+                        <Button variant="primary">Add to Cart</Button>
                     </Card.Body>
                 </Card>
             )
