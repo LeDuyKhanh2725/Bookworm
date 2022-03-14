a2c66283714d4ba3451bd7dd61c21dc7ec1aa94a
Duy Khanh <leduykhanh2725@fpt.edu.vn>
2022-03-14 22:29:51 +0700

    update content for about page

--- resources/js/pages/about/About.js
+++ resources/js/pages/about/About.js
@@ -11,29 +11,27 @@ function About() {
         <Row>
           <Col md={{ span: 6, offset: 3 }}>
             <h1>Wellcome to BookWorm</h1>
-            <p>"Mauris tempus erat laoreet turpis lobortis, eu tincidunt erat fermentum.
-              Aliquam non tincidunt urna. Integer tincidunt nec nisl vitae ullamcorper.
-              Proin sed ultrices erat. Praesent varius ultrices massa at faucibus.
-              Aenean dignissim, orci sed faucibus pharetra, dui mi dignissim tortor, sit amet condimentum mi ligula sit amet augue. Pellentesque vitae eros eget enim mollis placerat.
-              Aliquam non tincidunt urna. Integer tincidunt nec nisl vitae ullamcorper.
-              Proin sed ultrices erat. Praesent varius ultrices massa at faucibus.
-              Aenean dignissim, orci sed faucibus pharetra, dui mi dignissim tortor, sit amet condimentum mi ligula sit amet augue.
-              Pellentesque vitae eros eget enim mollis placerat."</p>
+            <p>"BookWorm is independent New York bookstore and language school with 
+            locations in Mahattan and Brooklyn. We specialize in travel books and language
+            classes"</p>
           </Col>
         </Row>
         <Row>
           <Col sm={3}></Col>
           <Col sm={3}>
-            <h4>Our Vision</h4>
-            <p>Pellentesque sodales augue eget ultricies ultricies. 
-            Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-            Curabitur sagittis ultrices condimentum.</p>
+            <h4>Our Story</h4>
+            <p>The name BookWorm was taken from the original name for New York International Airport
+            ,which was renamed JFK in December 1963. Our Mahattan store has just move to the West Village
+            . Our new location is 170 7th Avenue South, at the corner of Perry Street.
+            From March 2008 through May 2016, the store was located in the Flatiron District</p>
           </Col>
           <Col sm={3}>
-            <h4>Our Story</h4>
-            <p>Pellentesque sodales augue eget ultricies ultricies. 
-            Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-            Curabitur sagittis ultrices condimentum.</p>
+            <h4>Our Vision</h4>
+            <p>One of the last travel bookstores in the country, our Mahattan store carri a range of 
+            guildbooks( all 10% off) to suit the needs and tastes of every traveler and budget.
+            We believe that a novel or travelogue can be just as valuable a key to a place as any guildbooks
+            ,and our well-read, well-traveled staff is happy to make reading reccomendations for any traveler,
+            book lover, or gift giver </p>
           </Col>
           <Col sm={3}></Col>
         </Row>
