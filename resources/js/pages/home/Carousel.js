41fff7ca0e7f2d5eace5d3b272a126117a7ba210
Duy Khanh <leduykhanh2725@fpt.edu.vn>
2022-03-14 16:21:12 +0700

    add link for product(developing)

--- resources/js/pages/home/Carousel.js
+++ resources/js/pages/home/Carousel.js
@@ -13,6 +13,7 @@ import "./Carousel.css";
 // import required modules
 import { Pagination, Navigation } from "swiper";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Carousel({ list }) {
 
@@ -21,9 +22,13 @@ export default function Carousel({ list }) {
         return list.map((item) => {
             return (<SwiperSlide >
                 <Card className='col-md-3 me-3' style={{ width: '18rem' }}>
-                    <Card.Img height={'250px'} variant="top" src={"/bookcover/" + item.book_cover_photo + ".jpg"} />
+                <Link to={`/book-list/${item.book_id}`}><Card.Img height={'250px'} variant="top" 
+                    src={"/bookcover/" + item.book_cover_photo + ".jpg"} />
+                </Link>
                     <Card.Body>
-                        <Card.Title height={'50px'}>{item.book_title}</Card.Title>
+                    <Link to={`/book-list/${item.book_id}`}>
+                    <Card.Title height={'50px'}>{item.book_title}</Card.Title>
+                    </Link>
                         <Card.Text height={'50px'}>
                             {item.author_name}
                         </Card.Text>
--- resources/js/pages/shop/Product.js
+++ resources/js/pages/shop/Product.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import bootstrap from 'bootstrap'
 import { Button, Card, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import ProductDetail from './ProductDetail'
 
 function Product({ list }) {
     const renderListItem = (list = []) => {
         return list.map((item) => {
             return (
                 <Card height={'500px'} className='col-md-3 me-3' style={{ width: '18rem' }}>
-                    <Card.Img height={'250px'} variant="top" src={"/bookcover/" + item.book_cover_photo + ".jpg"} />
+                    <Link to={`/book-list/${item.book_id}`}><Card.Img height={'250px'} variant="top"
+                        src={"/bookcover/" + item.book_cover_photo + ".jpg"} /></Link>
                     <Card.Body>
-                        <Card.Title height={'50px'}>{item.book_title}</Card.Title>
+                        <Link to={`/book-list/${item.book_id}`}>
+                            <Card.Title height={'50px'}>{item.book_title}</Card.Title>
+                        </Link>
                         <Card.Text height={'50px'}>
                             {item.author_name}
                         </Card.Text>
                         <Card.Text height={'50px'}>
                             {item.book_price}/{item.discount_price}
                         </Card.Text>
-                        <Button variant="primary">Add to Cart</Button>
                     </Card.Body>
                 </Card>
             )
